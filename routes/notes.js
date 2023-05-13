54c647e32a36aa5c55f48ec9ce641d65642bc5b5
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-05-13 16:16:53 -0400

    finished most of the code and added a delete method.

--- routes/notes.js
+++ routes/notes.js
@@ -50,4 +50,31 @@ notes.post("/", (req, res) => {
   }
 });
 
+notes.delete("/:id", (req, res) => {
+  console.log(req.method);
+  const noteId = req.params.id;
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({error: 'Error reading notes file'});
+    } else {
+      const parsedData = JSON.parse(data);
+      const filteredNotes = parsedData.filter((note) => note.id !== noteId);
+      if (filteredNotes.length === parsedData.length) {
+        res.status(404).json({error: 'Note not found'});
+      } else {
+        fs.writeFile("./db/db.json", JSON.stringify(filteredNotes), (writeErr) => {
+          if (writeErr) {
+            console.error(writeErr);
+            res.status(500).json({error: 'Error writing to notes file'});
+          } else {
+            console.log(`Note with id ${noteId} was deleted`);
+            res.status(200).json({success: 'Note deleted successfully'});
+          }
+        });
+      }
+    }
+  });
+});
+
 module.exports = notes;
