2a0a8e607966763cbd8b88fa6a7d73b8e68c7578
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-05-15 13:36:56 -0400

    changed the port const to use heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const api = require("./routes/index.js");
-const PORT = 3001;
+const PORT = process.env.PORT|| 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
